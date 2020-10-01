f91d4ccdc09b05a70d80a51dce29b4fa2da28e4b
Raikas-JoinT <ketukoketuo@gmail.com>
2020-10-01 17:49:03 +0900

    ビューファイル設定

---
+++ app/javascript/background.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  const particles = Particles.init({
+    selector: '.background',
+  sizeVariations: 5,
+  color: ['#00FF7F', '#F0F8FF', '#231557', '#44107a', '#ff1361', '#fff800'],
+  connectParticles: true
+  });
+};
