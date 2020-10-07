6ea137738f395317e8a821d6d6d099bd2989405d
Raikas-JoinT <ketukoketuo@gmail.com>
2020-10-07 13:46:26 +0900

    文字数取得

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -4,12 +4,26 @@ class AffiliatesController < ApplicationController
   end
 
   def show
+    affiliate_keyword = Affiliate.find(params[:id]).keyword
+    driver = Selenium::WebDriver.for :chrome
+
+    driver.navigate.to "https://www.google.co.jp/"
+    inputElement = driver.find_element(:name, 'q')
+    inputElement.send_keys affiliate_keyword
+    inputElement.submit
+
+    search = driver.find_element(:css, "div.rc a")
+    search.click
+
+    content = driver.find_element(:css, "body")
+    @affiliate_keyword = content.text.length
+    driver.quit
   end
 
   def create
     @affiliate = Affiliate.new(affiliate_params)
     @affiliate.save
-      redirect_to affiliates_path
+      redirect_to affiliate_path(@affiliate)
   end
 
   private
