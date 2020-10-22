aa84f1fb46e80abcd031821981f6f3130061af0d
Raikas-JoinT <ketukoketuo@gmail.com>
2020-10-22 11:51:19 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: "affiliates#index"
-  resources :affiliates, only: [:index, :show, :create]
-  resources :messages, only: [:index, :create]
+  resources :affiliates
+  resources :messages
   get 'messages/index', to: 'messages#index'
 end
