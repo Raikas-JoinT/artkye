0e1d7e94be07e0938736b8eb853bdf2b988741d8
Raikas-JoinT <ketukoketuo@gmail.com>
2020-11-01 11:15:20 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   resources :affiliates, only: [:index, :show, :create]
   resources :messages, only: [:index, :create]
   get 'messages/search', to: 'messages#search'
+
+  put 'users/follow/:user_id',to: 'users#follow'
+  put 'users/unfollow/:user_id',to: 'users#unfollow'
+
+  get 'users/follow_list/:user_id',to: 'users#follow_list'
+  get 'users/follower_list/:user_id',to:'users#follower_list'
 end
